ff125d221002bbad21255a52ef7f6227ae8db198
Clamendeena S <79325932+Clamendeena@users.noreply.github.com>
2023-11-28 21:59:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
     const image = document.querySelector('.image');
     const scrollY = window.scrollY;
-    const transformValue = `translate(${scrollY}px, ${scrollY}px)`;
-    image.style.transform = transformValue;
+    const Value = `translate(${scrollY}px, ${scrollY}px)`;
+    image.style.transform = Value;
 });
